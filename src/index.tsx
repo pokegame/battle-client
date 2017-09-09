2cd8109378e32f2007f917168321ad04e6d1152e
Federkun <federkun@gmail.com>
2017-09-09 22:02:07 +0200

    Connect to battle-server and pass the socket to App Component

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as io from 'socket.io-client';
 import App from './App';
 import './index.css';
 
+const serverEndpoint = process.env.REACT_APP_SERVER_ENDPOINT || '/';
+const socket = io(serverEndpoint);
+
 ReactDOM.render(
-  <App />,
+  <App socket={socket} />,
   document.getElementById('root') as HTMLElement
 );
