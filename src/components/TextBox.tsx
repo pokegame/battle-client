7985db59a8ee285ef4afc1f965712e61c4bbd6f1
Federkun <federkun@gmail.com>
2017-09-21 09:49:50 +0100

    Add textbox component

---
+++ src/components/TextBox.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import './TextBox.css';
+
+const TextBox = ({ options }) => {
+  const optionsClassName = [
+    'battle-text-top-left',
+    'battle-text-top-right',
+    'battle-text-bottom-left',
+    'battle-text-bottom-right'
+  ];
+
+  const choices = options.map((choice, index) => (
+    <h4 className={optionsClassName[index]} onClick={choice.onClick}>{choice.text}</h4>
+  ));
+
+  return (
+    <div>
+      <div className="battle-text text-box-left">
+        @TODO
+      </div>
+      <div className="text-box-right">
+        <div>
+          {choices}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TextBox;
